b533587b3c4f6eb85ed5f7cf7bbbe42c5e5f45df
Tramy Nguyen <tramynn2@gmail.com>
2019-08-23 14:21:29 -0500

    completed step 4

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
-const products = require('../products.json');
+const getProducts = require('./getProducts');
 
 const port = 2020;
 
-app.get('/api/products', (req, res,) => {
-    res.status(200).send(products);
-});
+app.get('/api/products', getProducts);
 
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}`);
