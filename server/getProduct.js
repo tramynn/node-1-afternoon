ab9f1cbac812400cacb8db544cec54db8da7b9e1
Tramy Nguyen <tramynn2@gmail.com>
2019-08-26 15:16:46 -0500

    fixed step 5 again

--- server/getProduct.js
+++ server/getProduct.js
@@ -1,11 +1,16 @@
 const products = require('../products.json');
 
 const getProduct = (req, res,) => {
-    const item = products.filter(product => product.id === +req.params.id);
-    if (!item) {
+    // find returns item or undefined if not found
+    // filter returns an empty array if nothing found which will still be truthy
+    // it returns an array of objects if it finds anything 
+    const item = products.filter(product => product.id == +req.params.id);
+    console.log(item);
+    if (item.length === 0) {
         return res.status(500).send("Item not in list");
+    } else {
+        res.status(200).send(item);
     }
-    res.status(200).send(item);
 }
 
 module.exports = getProduct;
