569b59e91bd222304441d76891230de4036e8cd7
Tramy Nguyen <tramynn2@gmail.com>
2019-08-26 15:04:44 -0500

    fixed step 5

--- server/index.js
+++ server/index.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const app = express();
 const getProducts = require('./getProducts');
 const getProduct = require('./getProduct');
-
-const port = 2020;
+const app = express();
 
 app.get('/api/products', getProducts);
-app.get('/api/product:id', getProduct);
+app.get('/api/products/:id', getProduct);
 
-
-
+const port = 2020;
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}`);
 })
